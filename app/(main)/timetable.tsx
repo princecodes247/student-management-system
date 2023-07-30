b13f88057e530c86b212e5bc3b6e7d0500c5ece9
Prince Codes <princecodes247@gmail.com>
2023-07-30 22:24:07 +0100

    update timetable screen

--- app/(main)/timetable.tsx
+++ app/(main)/timetable.tsx
@@ -14,55 +14,26 @@ export default function Timetable() {
       <View className="flex-row justify-between gap-2">
         <View className="flex-1">
           <Picker
-            placeholder="Choose Department"
+            placeholder="Choose Level"
             items={[
-              { label: "Football", value: "football" },
-              { label: "Baseball", value: "baseball" },
-              { label: "Hockey", value: "hockey" },
+              { label: "100 level", value: "100l" },
+              { label: "200 level", value: "200l" },
+              { label: "300 level", value: "300l" },
             ]}
           />
         </View>
         <View className="flex-1">
           <Picker
-            placeholder="Select Level"
+            placeholder="Select Semester"
             items={[
-              { label: "Football", value: "football" },
-              { label: "Baseball", value: "baseball" },
-              { label: "Hockey", value: "hockey" },
+              { label: "1st semester", value: "1" },
+              { label: "2nd semester", value: "2" },
             ]}
           />
         </View>
       </View>
       <View className="flex-1 mt-6">
-        <View className="w-full p-6 mt-8 bg-gray-100 border rounded-xl">
-          <View className="flex-row gap-2">
-            <Text className="text-sm text-primary-foreground">
-              Upcoming Lecture
-            </Text>
-            <Text className="text-sm text-primary-foreground">2pm</Text>
-          </View>
-          <Text className="mt-1 text-xl text-primary-foreground">
-            EEE351 - Logic & Digital systems
-          </Text>
-          <View className="flex-row mt-8">
-            <Button
-              size="sm"
-              classNames="border-white"
-              variant="outline"
-              href="to"
-            >
-              <Text className="text-white">Set Reminder</Text>
-            </Button>
-            <Button
-              size="sm"
-              classNames="ml-2 border-white"
-              variant="outline"
-              href="to"
-            >
-              <Text className="text-white">Set Reminder</Text>
-            </Button>
-          </View>
-        </View>
+        <View className="w-full p-6 h-[180px] mt-8 bg-gray-100 border rounded"></View>
 
         <Text className="mt-6 text-gray-600">
           Note that this timetable is subject to change and it is best that you
